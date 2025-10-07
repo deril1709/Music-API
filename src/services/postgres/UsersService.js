054c64535ec2b4c0a8336c9221c4c6870aaf4f09
deril1709 <punktator17@gmail.com>
2025-10-07 17:16:45 +0800

    add  new exceptions

---
+++ src/services/postgres/UsersService.js
@@ -0,0 +1,43 @@
+const bcrypt = require('bcrypt');
+const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
+
+class UsersService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addUser({ username, password, fullname }) {
+    await this.verifyNewUsername(username);
+    const id = `user-${nanoid(16)}`;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const query = {
+      text: 'INSERT INTO users VALUES($1, $2, $3, $4)',
+      values: [id, username, hashedPassword, fullname],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new InvariantError('gagal menambahkan user');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async verifyNewUsername(username) {
+    const query = {
+      text: 'SELECT username FROM users WHERE username = $1',
+      values: [username],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (result.rows.length > 0) {
+      throw new InvariantError('Gagal menambahkan user, username sudah dipakai');
+    }
+  }
+}
+
+module.exports = UsersService;
